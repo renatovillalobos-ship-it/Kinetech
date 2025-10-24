92e08e7c9518ceb70b7a2cf072ab21988a7fe9e7
Pia-G05 <pia.gonzalez01@alumnos.ucn.cl>
2025-10-24 12:34:34 -0300

    Creación App Administrador

--- aplicacion/Webpage/settings/local.py
+++ aplicacion/Webpage/settings/local.py
@@ -10,8 +10,12 @@ ALLOWED_HOSTS = []
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'db_kine',
+        'USER': 'user_kine',
+        'PASSWORD': 'k01',
+        'HOST':'localhost',
+        'PORT': '5432'
     }
 }
 
