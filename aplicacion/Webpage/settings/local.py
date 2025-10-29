e408eadf13787d0d362df9c21c6abe5707d108e7
renatovillalobos-ship-it <renato.villalobos@alumnos.ucn.cl>
2025-10-28 22:34:01 -0300

    Versión Arreglada

--- aplicacion/Webpage/settings/local.py
+++ aplicacion/Webpage/settings/local.py
@@ -10,12 +10,8 @@ ALLOWED_HOSTS = []
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'db_kin',
-        'USER': 'user_kin',
-        'PASSWORD': 'k01',
-        'HOST':'localhost',
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
