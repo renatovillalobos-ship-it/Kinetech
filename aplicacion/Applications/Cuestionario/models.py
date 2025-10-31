061df653ae23393a431f33c8e21ac8fbd6abf966
Alexandra Herrera <alexaherreracl@gmail.com>
2025-10-30 23:37:29 -0300

    Tablas Cuestionario y Preguntas

--- aplicacion/Applications/Cuestionario/models.py
+++ aplicacion/Applications/Cuestionario/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from Applications.Docente.models import Curso
 
 # Create your models here.
+class cuestionario(models.Model):
+    Curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    nombre=models.CharField('Nombre Cuestionario',max_length=100, null=False)
+
+    def __str__(self):
+        return str(self.id)+'-'+self.nombre
+
+class Preguntas(models.Model):
+    cuestionario = models.ForeignKey(cuestionario, on_delete=models.CASCADE)
+    enunciado = models.TextField('Enunciado Pregunta', null=False )
+
+    
+    def __str__(self):
+        return str(self.id)+'-'+self.enunciado
+
+
