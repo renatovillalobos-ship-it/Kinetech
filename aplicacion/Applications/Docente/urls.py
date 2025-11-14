49c4db76388a860fea1b1308cd18d8e278711acd
icamiis <camila.ramos02@alumnos.ucn.cl>
2025-11-13 23:56:42 -0300

    Menú de docente
    Vista de menú de docente en la parte superior

--- aplicacion/Applications/Docente/urls.py
+++ aplicacion/Applications/Docente/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home_docente',views.Home_docente.as_view(),name='home_docente'),
-    path('login',views.Login.as_view(),name='login')
+    path('home_docente/',views.Home_docente.as_view(),name='home_docente'),
+    path('login/',views.Login.as_view(),name='login')
 ]
--- aplicacion/Applications/Docente/views.py
+++ aplicacion/Applications/Docente/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Curso 
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 from django.views.generic import (
@@ -12,3 +14,20 @@ class Login(TemplateView):
 
 class Home_docente(TemplateView):
     template_name='docente/home_docente.html'
+
+@login_required 
+def inicio_docente(request):
+
+    try:
+        docente_actual = request.user.docente
+    except:
+
+        return render(request, 'error_no_docente.html')
+
+    cursos_del_docente = Curso.objects.filter(docente=docente_actual)
+
+    contexto = {
+        'cursos': cursos_del_docente,
+    }
+    
+    return render(request, 'inicio_docente.html', contexto)
