76310bdb2dcb66c8e47b967388df7145d5c45dda
Pia-G05 <pia.gonzalez01@alumnos.ucn.cl>
2025-11-14 21:04:56 -0300

    Vista de Perfil Docente

--- aplicacion/Applications/Docente/urls.py
+++ aplicacion/Applications/Docente/urls.py
@@ -3,5 +3,13 @@ from . import views
 
 urlpatterns = [
     path('home_docente/',views.Home_docente.as_view(),name='home_docente'),
-    path('login/',views.Login.as_view(),name='login')
+    path('login/',views.Login.as_view(),name='login'),
+
+     # Rutas para la foto del docente
+    
+    path('perfil_docente/', views.PerfilDocente.as_view(), name='perfil_docente'),
+
+    path('subir_foto/<int:id>/', views.subir_foto_docente, name='subir_foto_docente'),
+    path('eliminar_foto/<int:id>/', views.eliminar_foto_docente, name='eliminar_foto_docente'),
+
 ]
--- aplicacion/Applications/Docente/views.py
+++ aplicacion/Applications/Docente/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Curso 
 from django.contrib.auth.decorators import login_required
+from .models import Docente
 
 # Create your views here.
 from django.views.generic import (
@@ -9,11 +10,68 @@ from django.views.generic import (
 
 )
 
+import os
+from django.contrib import messages
+from django.shortcuts import render, get_object_or_404, redirect
+
+
+
 class Login(TemplateView):
     template_name='login/login.html'
 
 class Home_docente(TemplateView):
     template_name='docente/home_docente.html'
+
+
+#gregue lo que esta abajo
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # Tomar el PRIMER docente (o el único existente)
+        context['docente'] = Docente.objects.first()
+
+        return context
+    
+# 1. VISTA NECESARIA PARA MOSTRAR TU PERFIL
+class PerfilDocente(TemplateView):
+    template_name='docente/perfil_docente.html' # Asegúrate de la ruta   
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['docente'] = Docente.objects.first()  # o el docente logueado
+        return context
+    
+    #ESTO PARA LA FOTO
+    
+def subir_foto_docente(request, id):
+    docente = get_object_or_404(Docente, id=id)
+
+    if request.method == 'POST':
+        foto = request.FILES.get('foto')
+        if foto:
+            docente.foto_perfil_docente = foto
+            docente.save()
+            messages.success(request, 'Foto actualizada correctamente.')
+
+    return redirect('perfil_docente')
+
+
+def eliminar_foto_docente(request, id):
+    docente = get_object_or_404(Docente, id=id)
+
+    if docente.foto_perfil_docente:
+            # Eliminar archivo físico
+        if os.path.isfile(docente.foto_perfil_docente.path):
+            os.remove(docente.foto_perfil_docente.path)
+        docente.foto_perfil_docente = None
+        docente.save()
+        messages.success(request, 'Foto eliminada correctamente.')
+
+    return redirect('perfil_docente')
+
+
+
+
 
 @login_required 
 def inicio_docente(request):
