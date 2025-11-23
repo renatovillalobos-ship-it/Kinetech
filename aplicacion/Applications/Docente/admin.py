66213deaaa493134b1c11d75ca984678cd157791
renatovillalobos-ship-it <renato.villalobos@alumnos.ucn.cl>
2025-11-23 18:13:44 -0300

    Update admin.py

--- aplicacion/Applications/Docente/admin.py
+++ aplicacion/Applications/Docente/admin.py
@@ -1,24 +1,68 @@
 from django.contrib import admin
 from django.utils.html import format_html
+from django import forms
+from django.contrib.auth.models import User
 from .models import *
 
+# -------------------------------
+# FORMULARIO PERSONALIZADO DOCENTE
+# -------------------------------
+class DocenteForm(forms.ModelForm):
+    correo = forms.EmailField(
+        label="Correo Electrónico",
+        widget=forms.TextInput(attrs={'class': 'vTextField', 'size': 32})  # Usar TextInput, no EmailInput
+    )
+    # resto igual...
 
+    class Meta:
+        model = Docente
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Inicializa el correo si el User ya existe
+        if self.instance and hasattr(self.instance, 'user') and self.instance.user:
+            self.fields['correo'].initial = self.instance.user.email
+
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+        # Crea User si no existe
+        if not hasattr(instance, 'user') or instance.user is None:
+            from django.contrib.auth.models import User
+            # Se puede crear un usuario temporal con username igual al nombre+apellido
+            username = f"{instance.nombre_docente.lower()}.{instance.apellido_docente.lower()}"
+            user = User.objects.create(username=username, email=self.cleaned_data['correo'])
+            instance.user = user
+        else:
+            # Actualiza el correo si el User ya existe
+            instance.user.email = self.cleaned_data['correo']
+            instance.user.save()
+
+        if commit:
+            instance.save()
+        return instance
+
+# -------------------------------
+# ADMIN DOCENTE
+# -------------------------------
 class DocenteAdmin(admin.ModelAdmin):
-    list_display=(
+    form = DocenteForm
+
+    list_display = (
         'nombre_docente',
         'apellido_docente',
         'obtener_correo',
         'foto_preview',
         'pais_docente',
     )
-    search_fields=('nombre_docente',)
+    search_fields = ('nombre_docente',)
     readonly_fields = ('foto_preview',) 
     fieldsets = (
         (None, {
             'fields': (
                 'nombre_docente',
                 'apellido_docente',
-                'obtener_correo',
+                'correo',            # Ahora editable
                 'pais_docente', 
                 'foto_perfil_docente',          
                 'foto_preview',   
@@ -27,8 +71,11 @@ class DocenteAdmin(admin.ModelAdmin):
     )
 
     def obtener_correo(self, obj):
-        # Accede al campo email del objeto User vinculado
-        return obj.user.email 
+        # Protege el acceso a user
+        try:
+            return obj.user.email
+        except (AttributeError, User.DoesNotExist):
+            return "No asignado"
 
     obtener_correo.short_description = 'Correo Electrónico'
 
@@ -40,13 +87,20 @@ class DocenteAdmin(admin.ModelAdmin):
             )
         return "Sin foto"
     foto_preview.short_description = "Foto de perfil"
+
+# -------------------------------
+# ADMIN CURSO
+# -------------------------------
 class CursoAdmin(admin.ModelAdmin):
-    list_display=(
+    list_display = (
         'id',
         'nombre_del_Curso',
         'fecha_realización_curso',
     )
-    search_fields=('nombre_del_Curso',)
-# Register your models here.
-admin.site.register(Docente,DocenteAdmin)
-admin.site.register(Curso,CursoAdmin)
+    search_fields = ('nombre_del_Curso',)
+
+# -------------------------------
+# REGISTER MODELS
+# -------------------------------
+admin.site.register(Docente, DocenteAdmin)
+admin.site.register(Curso, CursoAdmin)
